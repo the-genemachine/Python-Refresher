566bd2fb15a4ed4127e492d9c2ede28d9a87b1b5
the-genemachine <gene.gallant@rogers.com>
2024-05-12 00:44:56 -0400

    Added python code demonstrating bools and operators

--- Boolean and Operators/boolean-operators.py
+++ Boolean and Operators/boolean-operators.py
@@ -1,19 +1,39 @@
-"""
-Boolean and Operators
-"""
-
-# Comparison Operators
-print(1 == 2)
-print(1 != 2)
-print(1 > 2)
-print(1 < 2)
-print(1 >= 1)
-print(1 <= 2)
+import os
 
 
-# Logical Operators
-print(1 > 3 and 5 < 7)
-print(1 > 3 or 5 < 7)
-print(not(1 == 1))
+# Function to clear the console screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
+# Function to display a title
+def display_title(title):
+    clear_screen()
+    print("=" * len(title))
+    print(title)
+    print("=" * len(title))
+    print()
+
+
+# Title
+title = "Demonstrating the Use of Boolean Operators in Python"
+display_title(title)
+
+# Boolean AND operator
+print("Boolean AND Operator:")
+print("True and True:", True and True)
+print("True and False:", True and False)
+print("False and True:", False and True)
+print("False and False:", False and False)
+
+# Boolean OR operator
+print("\nBoolean OR Operator:")
+print("True or True:", True or True)
+print("True or False:", True or False)
+print("False or True:", False or True)
+print("False or False:", False or False)
+
+# Boolean NOT operator
+print("\nBoolean NOT Operator:")
+print("not True:", not True)
+print("not False:", not False)
