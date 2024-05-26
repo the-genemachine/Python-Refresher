e1c8e2d4fd3054d0080f04a89992d7ea89e44dcf
the-genemachine <gene.gallant@rogers.com>
2024-05-26 00:36:41 -0400

    Added a small program demonstrating imports

---
+++ Imports/calculator.py
@@ -0,0 +1,18 @@
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+
+# Function to perform division
+def divide(x, y):
+    return x / y if y != 0 else "Cannot divide by zero"
